4e9767f1723c933002e83d2f4a9ab8c1e797fa50
Irma Gerguri <irge24@student.bth.se>
2025-04-04 19:16:51 +0200

    Initial commit with project files

---
+++ src/Controller/LuckyControllerJson.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerJson extends AbstractController
+{
+    #[Route("/api", name: "api")]
+    public function api(): Response
+    {
+        return $this->render('api.html.twig');
+    }
+
+    #[Route("/api/quote", name: "api_quote")]
+    public function quote(): Response
+    {
+
+    $data = [
+        'quote1' => 'Be happy, be positive.',
+        'quote2' => 'Every situation is what you make out of it.',
+        'quote3' => 'Be strong!'
+    ];
+
+    $random_quote = array_rand($data, 1);
+
+    $date = date("l jS \of F Y h:i:s A");
+
+    $responseData = [
+        'date' => $date,
+        'quote' => $data[$random_quote]
+    ];
+
+    $response = new JsonResponse($responseData);
+    $response->setEncodingOptions(
+        $response->getEncodingOptions() | JSON_PRETTY_PRINT
+    );
+
+    return $response;
+    }
+}
