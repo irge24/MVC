f8fd82e77acc6925ca8d52f0c03f076df4d5d27f
Irma Gerguri <irge24@student.bth.se>
2025-10-13 18:57:34 +0200

    Spelplanen

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -28,9 +28,16 @@ class CardGameController extends AbstractController
 
     // Kmom03
     #[Route("/game/start", name: "start")]
-    public function start(): Response
+    public function start(SessionInterface $session): Response
     {
-        return $this->render('start.html.twig');
+        $data = [
+            "turn" => $session->get("turn") ?? "player",  // standard: player
+            "player_cards" => $session->get("player-cards") ?? [],
+            "bank_cards" => $session->get("bank-cards") ?? [],
+            "message" => ""
+        ];
+
+        return $this->render('start.html.twig', $data);
     }
 
     // Kmom03
@@ -38,6 +45,126 @@ class CardGameController extends AbstractController
     public function doc(): Response
     {
         return $this->render('doc.html.twig');
+    }
+
+    // Kmom03
+    #[Route("/game/draw_card", name: "draw_card")]
+    public function draw_card(SessionInterface $session): Response
+    {
+        
+        // Spelare tar ett kort från kortleken
+        if ($session->get("deck") === null) {
+            $deckObject = new DeckOfCards();
+            $deckObject->deck();
+            $session->set("deck", $deckObject->getDeck());
+        }
+        
+        $cardDeck = $session->get("deck");
+        $card = new Card();
+        $result = $card->aCard($cardDeck);
+        $aCard = $result[0];
+        $newDeck = $result[1];
+
+        // Uppdaterar kortleken, tar bort dragna kort
+        $session->set("current-deck", $newDeck);
+
+        // Vems tur det är
+        if ($session->get("turn") === null) {
+            $session->set("turn", "player");
+            $playerCards = [];
+            $bankCards = [];
+            $session->set("player-cards", $playerCards);
+            $session->set("bank-cards", $bankCards);
+        }
+        
+        if ($session->get("turn") === "player") {
+            $playerCards = $session->get("player-cards");
+            $playerCards[] = $aCard;
+            $session->set("player-cards", $playerCards);
+        } else {
+            $bankCards = $session->get("bank-cards");
+            $bankCards[] = $aCard;
+            $session->set("bank-cards", $bankCards);
+        }
+
+        // Kontrollera antal kort och totalt värde
+        $playerCards = $session->get("player-cards");
+        $bankCards = $session->get("bank-cards");
+
+        $totalPlayer = 0;
+        foreach ($playerCards as $card) {
+            $value = $card->getValue();
+            $totalPlayer += $value;
+        }
+
+        if ($totalPlayer == 21) {
+            $message = "";
+        } elseif ($totalPlayer > 21) {
+            $message = "Banken vann!";
+        } else {
+            // Fortsätt spela
+        }
+
+        $totalBank = 0;
+        foreach ($bankCards as $card) {
+            $value = $card->getValue();
+            $totalBank += $value;
+        }
+
+        if ($totalBank == 21) {
+            // Spelaren har blackjack!
+        } elseif ($totalBank > 21) {
+            $message = "Spelaren vann!";
+        } else {
+            // Fortsätt spela
+        }
+
+        $data = [
+            "aCard" => $aCard,
+            "player_cards" => $session->get("player-cards"),
+            "bank_cards" => $session->get("bank-cards"),
+            "turn" => $session->get("turn"),
+            "message" => $message ?? "",  // tom sträng om meddelande ej finns
+        ];
+
+        return $this->render('start.html.twig', $data);
+    }
+
+    // Kmom03
+    #[Route("/game/stop", name: "stop")]
+    public function stop(SessionInterface $session): Response
+    {
+        $turn = $session->get("turn");
+
+        if ($turn == "player") {
+            $session->set("turn", "bank");
+        }
+        else {
+            $session->set("turn", "player");
+        }
+
+        $data = [
+            "turn" => $session->get("turn")
+        ];
+
+        return $this->render('start.html.twig', $data);
+    }
+
+    // Kmom03
+    #[Route("/game/new_game", name: "new_game")]
+    public function new_game(SessionInterface $session): Response
+    {
+        // Nollställ spelet
+        $session->clear();
+
+        $data = [
+            "turn" => "player",
+            "player_cards" => [],
+            "bank_cards" => [],
+            "message" => "Ny omgång!"
+        ];
+
+        return $this->render('start.html.twig', $data);
     }
 
     #[Route("/session", name: "session")]
